4b1d090f03c935a0db889e6df56f955fdf62581a
lu-fer22 <luisamartinezjuarez077@gmail.com>
2026-06-29 16:08:07 -0600

    altas

--- altas.h
+++ altas.h
@@ -1,12 +1,13 @@
+#ifndef ALTAS_H
+#define ALTAS_H
+
 #include <stdio.h>
 #include <stdlib.h>
-#include "STRUCTS_H"
+#include <time.h>
+#include "struct.h"
+#include "menu.h"
 
-void altas(struct persona **ptr);
-struct persona *nuevaPersona(void);
-
-
-struct persona *nuevaPersona(void){
+static inline struct persona *nuevaPersona(void){
     struct persona *ptrTemp=NULL;
     ptrTemp = (struct persona *) malloc(sizeof(struct persona));
 
@@ -14,37 +15,129 @@ struct persona *nuevaPersona(void){
         printf("No se reservo memoria\n");
         return NULL;
     }else{
-        printf("Ingrese el nombre: ");
-        scanf("%s", &persona.nombre);
-        printf("Ingrese la edad: ");
-        scanf("%d", &persona);
-        printf("Ingrese el genero: ");
-        scanf("%s", &persona.genero);
-        printf("Ingrese la fecha de nacimiento: ");
-        scanf("%s", &persona.fn);
-
-        ptrtemp->edad=persona;
-        ptrtemp->sig=NULL;
+        ptrTemp->nombre = malloc(50 * sizeof(char));
+        printf("Nombre: ");
+        scanf("%49s", ptrTemp->nombre);
+        printf("Edad: ");
+        scanf("%d", &ptrTemp->edad);
+        printf("Genero(f/m): ");
+        scanf(" %c", &ptrTemp->genero);
+        printf("Fecha de nacimiento: ");
+        scanf("%7s", ptrTemp->fn);
+        ptrTemp->sig=NULL;
         
         return ptrTemp;
     }
 }
 
-void altas(struct persona **ptr){
-    struct persona *ptrAux;
-     ptrtemp=crear();
-        if(ptrtemp==NULL)
-            printf("No se creo el nuevo dato");
-        else{
-            if(ptr==NULL)
-                ptr=ptrtemp;
-            else{
-                ptraux=ptr;
-                    while(ptraux->sig!=NULL){
-                        ptraux=ptraux->sig;
-                        }
-                        ptraux->sig=ptrtemp;
-                }
+static inline struct alumno *nuevaAlumno(void){
+    struct alumno *ptrTemp=NULL;
+    ptrTemp = (struct alumno *) malloc(sizeof(struct alumno));
+
+    if(ptrTemp==NULL){
+        printf("No se reservo memoria\n");
+        return NULL;
+    }else{
+        printf("Matricula: ");
+        scanf("%9s", ptrTemp->matricula);
+        printf("Carrera: \n");
+        printf("D.S, I.F, L.B, C.A, L.A.T:\n");
+        scanf("%4s", ptrTemp->carrera);
+        printf("Semestre: ");
+        scanf("%d", &ptrTemp->semestre);
+        printf("Correo: ");
+        scanf("%22s", ptrTemp->correo);
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j < 5; j++){
+                ptrTemp->calif[i][j] = (rand() % 101) / 10.0;
             }
+        }
+        
+        return ptrTemp;
+    }
+}
+
+static inline void altas(struct persona **ptr){
+    struct persona *ptrTemp;
+    struct persona *ptrAux;
+    struct alumno *ptrTempA;
+    int op;
+
+    do{
+        op=menuAltas();
+
+        switch(op){
+            case 1: 
+                ptrTemp = nuevaPersona();
+                ptrTempA = nuevaAlumno();
+                if(ptrTemp == NULL){        
+                    printf("No se creo el nuevo dato\n");
+                }
+                else{
+                    if(ptrTempA == NULL){
+                        printf("No se creo el alumno\n");
+                        free(ptrTemp->nombre);
+                        free(ptrTemp);
+                    }
+                    else{
+                        ptrTemp->ptrAlum = ptrTempA;
+
+                        ptrTemp->sig = NULL;
+
+                        if(*ptr == NULL)
+                            *ptr = ptrTemp;
+                        else{
+                            ptrAux = *ptr;
+                            while(ptrAux->sig != NULL){
+                                ptrAux = ptrAux->sig;
+                            }
+                            ptrAux->sig = ptrTemp;
+                        }
+                    }
+                }
+                break;
+            case 2:
+                char opc;
+                do{
+                    ptrTemp = nuevaPersona();
+                    ptrTempA = nuevaAlumno();
+
+                    if(ptrTemp == NULL){
+                        printf("No se creo el nuevo dato\n");
+                    }
+                    else{
+                        if(ptrTempA == NULL){
+                            printf("No se creo el alumno\n");
+                            free(ptrTemp->nombre);
+                            free(ptrTemp);
+                        }
+                        else{
+                            ptrTemp->ptrAlum = ptrTempA;
+
+                            ptrTemp->sig = NULL;
+
+                            if(*ptr == NULL)
+                                *ptr = ptrTemp;
+                            else{
+                                ptrAux = *ptr;
+                                while(ptrAux->sig != NULL){
+                                    ptrAux = ptrAux->sig;
+                                }
+                                ptrAux->sig = ptrTemp;
+                            }
+                        }
+                    }
+                    printf("Desea agregar a otra persona (s/n)?");
+                    scanf(" %c", &opc);
+                }while(opc == 's' || opc == 'S');
+                break;
+            default: 
+                printf("Opcion no valida\n");
+                break;
+        }
+
+    }while(op!=0);
 
 }
+
+#endif

af61e198340cdf7df219a7ef535421bf114c8c77
lu-fer22 <luisamartinezjuarez077@gmail.com>
2026-06-29 16:09:07 -0600

    menu

--- menu.h
+++ menu.h
@@ -1,16 +1,31 @@
+#ifndef MENU_H
+#define MENU_H
+
 #include <stdio.h>
+#include "struct.h"
 
-int menuPrincipal(void);
-
-int menuPrincipal(void) {
+static inline int menuPrincipal() {
     int op;
-
     printf("Seleccione una opcion: \n");
-    printf("1.- Insertar\n");
-    printf("2.- Mostrar\n");
-    printf("3.- Liberar\n");
+    printf("1.- Altas\n");
+    printf("2.- Bajas\n");
+    printf("3.- Mostrar\n");
     printf("4.- Funciones Especiales\n");
     printf("0.- Salir\n");
     scanf("%d", &op);
     return op;
 }
+
+static inline int menuAltas(){
+    int op;
+    printf("\nALTAS\n");
+    printf("1.- Una persona\n");
+    printf("2.- Varias personas\n");
+    printf("0.- Salir\n");
+    scanf("%d", &op);
+    return op;
+}
+
+
+
+#endif

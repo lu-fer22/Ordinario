eadf3c0fcbcb1ab6b9bc2a9292a9176f1af68e2d
lu-fer22 <luisamartinezjuarez077@gmail.com>
2026-06-29 12:38:39 -0600

    menu.h

---
+++ menu.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int menuPrincipal(void);
+
+int menuPrincipal(void) {
+    int op;
+
+    printf("Seleccione una opcion: \n");
+    printf("1.- Insertar\n");
+    printf("2.- Mostrar\n");
+    printf("3.- Liberar\n");
+    printf("4.- Funciones Especiales\n");
+    printf("0.- Salir\n");
+    scanf("%d", &op);
+    return op;
+}

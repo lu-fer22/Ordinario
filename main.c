ab74beac7a1fbbe6df2eee8538d48a1acf4c85b2
lu-fer22 <luisamartinezjuarez077@gmail.com>
2026-06-30 02:22:52 -0600

    Refactor main function and include salida
    Removed redundant code and added salida function call.

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #include "bajas.h"
 #include "mostrar.h"
 #include "funEsp.h"
+#include "salida.h"
 
 int main(void){
     int op;
@@ -26,41 +27,7 @@ int main(void){
                 funEsp(&ptr);
                 break;
             case 0:
-                printf("Hasta luego.\n");
-                break;
-            default: 
-                printf("Opcion no valida\n");
-                break;
-        }
-    }while(op!=0);
-}#include <stdio.h>
-#include <stdlib.h>
-#include "menu.h"
-#include "altas.h"
-#include "struct.h"
-#include "bajas.h"
-#include "mostrar.h"
-#include "funEsp.h"
-
-int main(void){
-    int op;
-    struct persona *ptr=NULL;
-    srand(time(NULL));
-
-    do{
-        op=menuPrincipal();
-
-        switch(op){
-            case 1: 
-                altas(&ptr);
-                break;
-            case 2:
-                bajas(&ptr);
-                break;
-            case 3:
-                funEsp(&ptr);
-                break;
-            case 0:
+                salida(&ptr);
                 printf("Hasta luego.\n");
                 break;
             default: 

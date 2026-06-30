900bcf74bd51eb3e0562e7ffa98e5a426e018ab3
lu-fer22 <luisamartinezjuarez077@gmail.com>
2026-06-30 00:45:01 -0600

    Add multiple menu functions for user options

--- menu.h
+++ menu.h
@@ -51,4 +51,84 @@ static inline int menuMostrar(){
     return op;
 }
 
+static inline int menuFe(){
+    int op;
+    printf("\nFUNCIONES ESPECIALES\n");
+    printf("1.- Modificar\n");
+    printf("2.- Calificaciones\n");
+    printf("0.- Salir\n");
+    scanf("%d", &op);
+    return op;
+}
+
+static inline int menuModificar(){
+    int op;
+    printf("\nMODIFICAR\n");
+    printf("1.- calificaciones\n");
+    printf("2.- Datos\n");
+    printf("0.- Salir\n");
+    scanf("%d", &op);
+    return op;
+}
+
+static inline int menuModical(){
+    int op;
+    printf("\nMODIFICAR CALIFICACIONES\n");
+    printf("1.- Parcial\n");
+    printf("2.- Materia\n");
+    printf("0.- Salir\n");
+    scanf("%d", &op);
+    return op;
+}
+
+static inline int menuDatos(){
+    int op;
+    printf("\nMODIFICAR DATOS\n");
+    printf("1.- Persona\n");
+    printf("2.- Alumno\n");
+    printf("0.- Salir\n");
+    scanf("%d", &op);
+    return op;
+}
+
+static inline int menuCalif(){
+    int op;
+    printf("\nCALIFICACIONES\n");
+    printf("1.- Semestre\n");
+    printf("2.- Carrera\n");
+    printf("0.- Salir\n");
+    scanf("%d", &op);
+    return op;
+}
+
+static inline int menuSemestre(){
+    int op;
+    printf("\nCALIFICACIONES SEMESTRE\n");
+    printf("1.- Materia\n");
+    printf("2.- Parcial\n");
+    printf("0.- Salir\n");
+    scanf("%d", &op);
+    return op;
+}
+
+static inline int menuMateria(){
+    int op;
+    printf("\nCALIFICACIONES SEMESTRE MATERIA\n");
+    printf("1.- Aprovados y No aprobados\n");
+    printf("2.- Aprobaron parcial\n");
+    printf("0.- Salir\n");
+    scanf("%d", &op);
+    return op;
+}
+
+static inline int menuCarrera(){
+    int op;
+    printf("\nCALIFICACIONES CARRERA\n");
+    printf("1.- Aprobados\n");
+    printf("2.- De 1 a 3 adeudos\n");
+    printf("3.- Mas de 4 adeudos\n");
+    printf("0.- Salir\n");
+    scanf("%d", &op);
+    return op;
+}
 #endif

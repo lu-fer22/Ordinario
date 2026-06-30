0a5eabde6f23c88b24642ce52675dd4bded61d3c
lu-fer22 <luisamartinezjuarez077@gmail.com>
2026-06-30 00:48:44 -0600

    Refactor main function and include necessary headers

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include "struct.h"
 #include "bajas.h"
 #include "mostrar.h"
+#include "funEsp.h"
 
 int main(void){
     int op;
@@ -22,6 +23,42 @@ int main(void){
                 bajas(&ptr);
                 break;
             case 3:
+                funEsp(&ptr);
+                break;
+            case 0:
+                printf("Hasta luego.\n");
+                break;
+            default: 
+                printf("Opcion no valida\n");
+                break;
+        }
+    }while(op!=0);
+}#include <stdio.h>
+#include <stdlib.h>
+#include "menu.h"
+#include "altas.h"
+#include "struct.h"
+#include "bajas.h"
+#include "mostrar.h"
+#include "funEsp.h"
+
+int main(void){
+    int op;
+    struct persona *ptr=NULL;
+    srand(time(NULL));
+
+    do{
+        op=menuPrincipal();
+
+        switch(op){
+            case 1: 
+                altas(&ptr);
+                break;
+            case 2:
+                bajas(&ptr);
+                break;
+            case 3:
+                funEsp(&ptr);
                 break;
             case 0:
                 printf("Hasta luego.\n");
